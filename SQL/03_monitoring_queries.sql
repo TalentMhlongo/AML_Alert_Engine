7d23b2988c8d28a5a2a2902d09be530daffe59ff
TalentMhlongo <talentmhlongo15@gmail.com>
2026-04-06 17:04:22 +0200

    Update 03_monitoring_queries.sql

--- SQL/03_monitoring_queries.sql
+++ SQL/03_monitoring_queries.sql
@@ -1,1 +1,15 @@
-
+SELECT
+    d.deposit_id,
+    d.customer_id,
+    c.first_name,
+    c.last_name,
+    c.risk_rating,
+    d.deposit_date,
+    d.amount,
+    d.payment_method,
+    d.source_channel
+FROM HWB_deposits d
+JOIN HWB_customers c
+    ON d.customer_id = c.customer_id
+WHERE d.amount > 50000
+ORDER BY d.amount DESC;
