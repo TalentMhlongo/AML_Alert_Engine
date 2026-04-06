83982333e5d8058b62a6abfcfb6bed8134212f39
TalentMhlongo <talentmhlongo15@gmail.com>
2026-04-06 17:57:23 +0200

    Update config.py

--- python/config.py
+++ python/config.py
@@ -1,1 +1,14 @@
+import os
 
+# Load from environment variables (SAFE)
+DB2_HOST = os.getenv("DB2_HOST")
+DB2_PORT = os.getenv("DB2_PORT")
+DB2_NAME = os.getenv("DB2_NAME")
+DB2_USER = os.getenv("DB2_USER")
+DB2_PASSWORD = os.getenv("DB2_PASSWORD")
+
+# Tables
+CUSTOMERS_TABLE = "HWB_CUSTOMERS"
+DEPOSITS_TABLE = "HWB_DEPOSITS"
+
+THRESHOLD = 50000
