6c8f69379409353111efa964027b6c2615e6254b
TalentMhlongo <talentmhlongo15@gmail.com>
2026-04-08 09:57:06 +0200

    Update load_data.py

--- python/load_data.py
+++ python/load_data.py
@@ -1,1 +1,37 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from config import (
+    DATA_SOURCE,
+    CUSTOMERS_FILE,
+    DEPOSITS_FILE,
+    DB2_HOST,
+    DB2_PORT,
+    DB2_NAME,
+    DB2_USER,
+    DB2_PASSWORD,
+    CUSTOMERS_TABLE,
+    DEPOSITS_TABLE,
+)
 
+def load_from_csv():
+    customers = pd.read_csv(CUSTOMERS_FILE)
+    deposits = pd.read_csv(DEPOSITS_FILE)
+    return customers, deposits
+
+def get_db_engine():
+    connection_string = (
+        f"ibm_db_sa://{DB2_USER}:{DB2_PASSWORD}"
+        f"@{DB2_HOST}:{DB2_PORT}/{DB2_NAME}"
+    )
+    return create_engine(connection_string)
+
+def load_from_db():
+    engine = get_db_engine()
+    customers = pd.read_sql(f"SELECT * FROM {CUSTOMERS_TABLE}", engine)
+    deposits = pd.read_sql(f"SELECT * FROM {DEPOSITS_TABLE}", engine)
+    return customers, deposits
+
+def load_all_data():
+    if DATA_SOURCE.lower() == "db":
+        return load_from_db()
+    return load_from_csv()
