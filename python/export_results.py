b6887d55305f74b830acbb7fb798addff917e2db
TalentMhlongo <talentmhlongo15@gmail.com>
2026-04-08 09:58:55 +0200

    Update export_results.py

--- python/export_results.py
+++ python/export_results.py
@@ -1,1 +1,15 @@
+import os
+from config import PROCESSED_DATA_PATH
 
+def export_results(flagged_transactions, customer_summary):
+    os.makedirs(PROCESSED_DATA_PATH, exist_ok=True)
+
+    flagged_transactions.to_csv(
+        os.path.join(PROCESSED_DATA_PATH, "flagged_transactions.csv"),
+        index=False
+    )
+
+    customer_summary.to_csv(
+        os.path.join(PROCESSED_DATA_PATH, "customer_risk_summary.csv"),
+        index=False
+    )
